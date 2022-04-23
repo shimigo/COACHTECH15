5efdaa17c059e7f724de727a90bf0650e6a4df6b
shimigo <shimigo1965@yahoo.co.jp>
2022-04-23 20:54:56 +0900

    first-commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <p v-if="$auth.loggedIn">Name：{{ $auth.user.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
